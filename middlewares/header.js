f141f06330581beb47b93692bd2361ec035521f3
Joel Odufu <joel.odufu@gmail.com>
2022-03-07 08:12:29 +0100

    Update header.js

--- middlewares/header.js
+++ middlewares/header.js
@@ -20,8 +20,6 @@ headers.use((req, res, next) => {
         "Access-Control-Allow-Methods",
         "GET, POST, PATCH,PUT, DELETE, OPTIONS"
     );
-      res.disable("server");
-    res.disable("via");
     next();
 });
 
